adffb11edeaa4ca76535721dad327ded9bb78a63
Mirjam Jesmin <mirjam.jesmin@gmail.com>
2022-12-12 01:33:19 +0200

    R fail graafiku tegemisega

--- andmeteadus.R
+++ andmeteadus.R
@@ -12,9 +12,10 @@ asukoht2 <- asukoht1 %>% mutate(Elukoht = str_replace(Elukoht, "\\.\\.", ""))
 asukoht3 <- asukoht2 %>% mutate(Elukoht = tolower(Elukoht))
 asukoht4 <- asukoht3 %>% filter(grepl("maakond", Elukoht)) %>% select(Elukoht, Vanus.kokku)
 asukoht5 <- asukoht3 %>% select(Elukoht, Vanus.kokku)
-andmed <-  rbind(andmed1, andmed2)
-andmed1.1 <- andmed %>% select(!JuhtumId) %>% group_by_all() %>% summarise(n = n()) # kontrollin kas on ikka erinevad 
-andmed1.2 <- andmed1.1 %>%  group_by(KohtNimetus) %>% summarise(n = n())
+andmed <- read.csv2("clean_data.csv", sep = ",")
+#andmed <-  rbind(andmed1, andmed2)
+andmed1.1 <- andmed  %>% group_by_all() %>% summarise(n = n()) # kontrollin kas on ikka erinevad 
+andmed1.2 <- andmed %>%  group_by(KohtNimetus) %>% summarise(n = n())
 andmed1.3 <- andmed1.1 %>%  group_by(KohtLiik) %>% summarise(n = n())
 andmed1.4 <- andmed1.1 %>%  group_by(SyndmusLiik, KohtLiik) %>% summarise(n = n())
 andmed1.5 <-  andmed1.1 %>% mutate(Elukoht = tolower(MaakondNimetus))
@@ -46,6 +47,31 @@ andmed1.28 <-  pivot_wider(andmed1.27, names_from = SyyteoLiik, values_from = mi
 andmed1.29 <- andmed1.28 %>% mutate(osakaal_Väär = VT/kokku, osakaal_Kuri = KT/kokku)
 andmed1.30 <-  andmed %>% group_by(KohtLiik) %>% summarise(mitu = n())
   
+andmed1.31 <- andmed %>% group_by(SyyteoLiik) %>% summarise(mitu = n())
+andmed1.32 <- andmed %>% group_by(SyyteoLiik,SyndmusLiik) %>% summarise(mitu = n())
+andmed1.33 <- andmed %>% group_by(Kahjusumma) %>% summarise(mitu = n())
+
+tunniga <- andmed %>% mutate(hour = format(as.POSIXct(ToimKell,format="%H:%M"),"%H"))
+tunniga2 <-  tunniga %>% group_by(hour) %>% summarise(mitu = n())
+tunniga3 <-  tunniga %>% group_by(hour, SyyteoLiik) %>% summarise(mitu = n())
+
+41422+ 37872
+
+41422/79294
+
+andmed1.7 %>% ggplot(aes(x=MaakondNimetus, y = osakaal)) + geom_bar(stat="identity") + theme_bw() 
+tunniga2 %>% ggplot(aes(x=hour, y = mitu)) + geom_bar(stat="identity") + theme_bw()
+tunniga3 %>% ggplot(aes(x=hour, y = mitu, fill = SyyteoLiik)) + geom_bar(position = position_dodge(width = 0.8),stat="identity") + 
+  theme_bw() + xlab("Kellaaeg") + ylab("") + scale_fill_manual(values=c("#2E86C1", "#BB8FCE"), name="",
+                                                               labels=c("Kuritegu", "Väärtegu"))#+
+
+
+#  theme(legend.title=element_blank(),labels=c("Kuritegu", "Väärtegu"))
+
+
+res <- cor(andmed)
+round(res, 2)
+
 #c("Harjumaa","Hiiumaa","Ida-Virumaa","Jõgevamaa",
 #"Järvamaa","Läänemaa","Lääne-Virumaa","Pärnumaa",
 #"Põlvamaa","Raplamaa","Saaremaa","Tartumaa","Valgamaa","Viljandi","Võrumaa")
